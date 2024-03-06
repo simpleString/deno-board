274711375424e6cccbe661a42a2d69a1f2beed17
Volkov Dmitry <vdmitry.log@gmail.com>
2024-03-06 21:31:30 +0700

    test metadata

--- src/env.js
+++ src/env.js
@@ -53,7 +53,7 @@ export const env = createEnv({
     NEXTAUTH_URL: process.env.NEXTAUTH_URL,
     GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
     GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
-    NEXT_PUBLIC_DOMAIN: process.env.DOMAIN,
+    NEXT_PUBLIC_DOMAIN: process.env.NEXT_PUBLIC_DOMAIN,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
